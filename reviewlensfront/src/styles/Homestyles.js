8203b665e259313112b1775c360d168f754f3d67
Eunbin Ko <155472281+igbinni143@users.noreply.github.com>
2025-01-19 21:38:10 +0900

    DESIGN 크기 오류 해결

--- reviewlensfront/src/styles/Homestyles.js
+++ reviewlensfront/src/styles/Homestyles.js
@@ -1,124 +1,124 @@
-import { NavLink } from 'react-router-dom';
-import styled from 'styled-components';
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
 export const Body = styled.div`
-	margin: 0;
-	top: 0;
-	padding-bottom: 160px;
-	padding-left: 30px;
-	/*background-color: var(--primary-color);*/
-	background: linear-gradient(180deg, #ebf4ff 0%, #ffffff 100%);
-	width: 100vw;
-	height: 100vh;
-	padding-top: 100px;
-	box-sizing: border-box;
+  margin: 0;
+  top: 0;
+  padding-bottom: 160px;
+  padding-left: 30px;
+  /*background-color: var(--primary-color);*/
+  background: linear-gradient(180deg, #ebf4ff 0%, #ffffff 100%);
+  width: 100vw;
+  height: 100vh;
+  padding-top: 100px;
+  box-sizing: border-box;
 `;
 
 export const ContentBox = styled.div`
-	display: flex;
-	margin: auto;
-	margin-top: 120px;
-	align-items: center;
-	justify-content: center;
-	width: 50vw;
-	height: 40vh;
-	border: 0.5px solid shadow;
-	box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.5); /* 흐림 효과 그림자 */
-	border-radius: 10px;
-	/*background-color: var(--primary-color);*/
-	background-color: white;
+  display: flex;
+  margin: auto;
+  margin-top: 120px;
+  align-items: center;
+  justify-content: center;
+  width: 50vw;
+  height: clamp(450px, 40vh, 700px); /* 최소 500px, 기본 40vh, 최대 700px */
+  border: 0.5px solid shadow;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.5); /* 흐림 효과 그림자 */
+  border-radius: 10px;
+  /*background-color: var(--primary-color);*/
+  background-color: white;
 `;
 
 export const ContentBox2 = styled.div`
-	display: flex;
-	margin: auto;
-	margin-top: 120px;
-	align-items: center;
-	justify-content: center;
-	width: 40vw;
-	height: 60vh;
-	border: 0.5px solid shadow;
-	box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.5); /* 흐림 효과 그림자 */
-	border-radius: 10px;
-	/*background-color: var(--primary-color);*/
-	background-color: white;
+  display: flex;
+  margin: auto;
+  margin-top: clamp(80px, 6vh, 100px);
+  align-items: center;
+  justify-content: center;
+  width: 40vw;
+  height: clamp(650px, 60vh, 1100px); /* 최소 700px, 기본 60vh, 최대 1100px */
+  border: 0.5px solid shadow;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.5); /* 흐림 효과 그림자 */
+  border-radius: 10px;
+  /*background-color: var(--primary-color);*/
+  background-color: white;
 `;
 
 export const StyledNavLink = styled(NavLink)`
-	font-size: 18px;
-	text-decoration: none;
-	padding: 5px;
-	padding-bottom: 10px;
-	margin: 30px;
-	margin-top: 32px;
-	color: black;
-	display: flex; /* Flexbox 활성화 */
-	flex-direction: row; /* 세로 방향 정렬 */
-	align-items: center; /* 가로 축 가운데 정렬 */
-	justify-content: center; /* 세로 축 가운데 정렬 */
+  font-size: 18px;
+  text-decoration: none;
+  padding: 5px;
+  padding-bottom: 10px;
+  margin: 30px;
+  margin-top: 32px;
+  color: black;
+  display: flex; /* Flexbox 활성화 */
+  flex-direction: row; /* 세로 방향 정렬 */
+  align-items: center; /* 가로 축 가운데 정렬 */
+  justify-content: center; /* 세로 축 가운데 정렬 */
 
-	&.active {
-		/*font-weight: bold;*/
-	}
+  &.active {
+    /*font-weight: bold;*/
+  }
 
-	&:hover {
-		color: var(--primary-color);
-	}
+  &:hover {
+    color: var(--primary-color);
+  }
 
-	${({ isActive }) =>
-		isActive &&
-		`
+  ${({ isActive }) =>
+    isActive &&
+    `
       font-weight: 800;
       color: var(--primary-color);
     `}
 
-	&.main {
-		font-size: 35px;
-	}
+  &.main {
+    font-size: 35px;
+  }
 
-	&.login,
-	&.logout {
-		border: 1px solid black;
-		border-radius: 30px;
-		padding: 0 20px;
-		align-content: center;
-		font-weight: 800;
-		box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.3); /* 위쪽 방향 그림자 */
-	}
+  &.login,
+  &.logout {
+    border: 1px solid black;
+    border-radius: 30px;
+    padding: 0 20px;
+    align-content: center;
+    font-weight: 800;
+    box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.3); /* 위쪽 방향 그림자 */
+  }
 `;
 
 export const NavHeader = styled.div`
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	width: 100%;
-	height: 75px;
-	padding: 10px;
-	background-color: #f8f8f8;
-	position: fixed;
-	top: 40px;
-	left: 0;
-	box-sizing: border-box;
-	border-bottom: 0.08px solid;
-	box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.3);
-	z-index: 1000;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 75px;
+  padding: 10px;
+  background-color: #f8f8f8;
+  position: fixed;
+  top: 40px;
+  left: 0;
+  box-sizing: border-box;
+  border-bottom: 0.08px solid;
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.3);
+  z-index: 1000;
 `;
 
 export const Navpart2 = styled.div`
-	display: flex;
-	padding: 10px;
-	gap: 1px;
+  display: flex;
+  padding: 10px;
+  gap: 1px;
 `;
 
 export const Blank = styled.div`
-	display: flex;
-	height: 40px;
-	width: 100vw;
-	font-size: 16px;
-	background-color: #81baf9;
-	align-items: center;
-	justify-content: center;
-	top: 0px;
-	position: fixed;
-	z-index: 1000;
+  display: flex;
+  height: 40px;
+  width: 100vw;
+  font-size: 16px;
+  background-color: #81baf9;
+  align-items: center;
+  justify-content: center;
+  top: 0px;
+  position: fixed;
+  z-index: 1000;
 `;
--- reviewlensfront/src/styles/LoginBoxstyles.js
+++ reviewlensfront/src/styles/LoginBoxstyles.js
@@ -1,103 +1,103 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const LoginBoxs = styled.div`
-	display: flex;
-	align-items: left;
-	flex-direction: column;
-	font-size: 2rem;
-	font-weight: 900;
-	justify-content: center;
+  display: flex;
+  align-items: left;
+  flex-direction: column;
+  font-size: 2rem;
+  font-weight: 900;
+  justify-content: center;
 
-	p {
-		font-size: 1rem;
-		font-weight: 500;
-		gap: 0;
-	}
+  p {
+    font-size: 1rem;
+    font-weight: 500;
+    gap: 0;
+  }
 
-	a {
-		margin-left: 10px;
-		font-size: 1rem;
-		color: #81baf9;
-		cursor: pointer;
-	}
+  a {
+    margin-left: 10px;
+    font-size: 1rem;
+    color: #81baf9;
+    cursor: pointer;
+  }
 `;
 export const SignUpBoxs = styled.div`
-	display: flex;
-	align-items: left;
-	flex-direction: column;
-	font-size: 30px;
-	font-weight: 900;
-	justify-content: center;
-	margin-top: 50px;
-	margin-bottom: 50px;
-	gap: 20px;
+  display: flex;
+  align-items: left;
+  flex-direction: column;
+  font-size: 30px;
+  font-weight: 900;
+  justify-content: center;
+  margin-top: 0;
+  margin-bottom: 0px;
+  gap: 20px;
 
-	p {
-		font-size: 20px;
-		font-weight: 500;
-		gap: 0;
-	}
+  p {
+    font-size: 20px;
+    font-weight: 500;
+    gap: 0;
+  }
 
-	a {
-		margin-left: 10px;
-		font-size: 18px;
-		color: #81baf9;
-		cursor: pointer;
-	}
+  a {
+    margin-left: 10px;
+    font-size: 18px;
+    color: #81baf9;
+    cursor: pointer;
+  }
 `;
 
 export const IdBox = styled.div`
-	display: flex;
-	/*gap: 20px;*/
-	align-items: center;
-	justify-content: space-between;
-	margin: 10px;
-	width: 100%;
-	font-weight: 500;
-	font-size: 1.1rem;
+  display: flex;
+  /*gap: 20px;*/
+  align-items: center;
+  justify-content: space-between;
+  margin: 10px;
+  width: 100%;
+  font-weight: 500;
+  font-size: 1.1rem;
 `;
 export const IdInput = styled.input`
-	display: flex;
-	align-items: center;
-	height: 25px;
-	width: 20rem;
-	border: 0.01px solid #ccc; /* 테두리 추가 */
-	box-shadow: 2px 1px 3px rgba(0, 0, 0, 0.1);
-	outline: none;
-	font-size: 1.05rem;
-	border-radius: 8px;
-	margin-left: 2rem;
-	padding: 0.8rem;
-	padding-left: 10px;
+  display: flex;
+  align-items: center;
+  height: 25px;
+  width: 20rem;
+  border: 0.01px solid #ccc; /* 테두리 추가 */
+  box-shadow: 2px 1px 3px rgba(0, 0, 0, 0.1);
+  outline: none;
+  font-size: 1.05rem;
+  border-radius: 8px;
+  margin-left: 2rem;
+  padding: 0.8rem;
+  padding-left: 10px;
 `;
 
 export const ParentLoginBox = styled.div`
-	display: flex;
-	justify-content: flex-end;
+  display: flex;
+  justify-content: flex-end;
 `;
 export const LoginButton = styled.button`
-	width: 76px;
-	height: auto;
-	font-size: 18px;
-	font-weight: 500;
-	/*background-color: var(--primary-color);*/
-	background-color: #81baf9;
-	box-shadow: 1px 2px 10px rgba(0, 0, 0, 0.2);
-	color: white;
-	padding: 6.5px;
-	margin-top: 5px;
-	cursor: pointer;
+  width: 76px;
+  height: auto;
+  font-size: 18px;
+  font-weight: 500;
+  /*background-color: var(--primary-color);*/
+  background-color: #81baf9;
+  box-shadow: 1px 2px 10px rgba(0, 0, 0, 0.2);
+  color: white;
+  padding: 6.5px;
+  margin-top: 5px;
+  cursor: pointer;
 `;
 export const SignUpButton = styled.button`
-	width: 85px;
-	height: auto;
-	font-size: 18px;
-	font-weight: 500;
-	/*background-color: var(--primary-color);*/
-	background-color: #81baf9;
-	box-shadow: 1px 2px 10px rgba(0, 0, 0, 0.2);
-	color: white;
-	padding: 6.5px;
-	margin-top: 5px;
-	cursor: pointer;
+  width: auto;
+  height: auto;
+  font-size: 18px;
+  font-weight: 500;
+  /*background-color: var(--primary-color);*/
+  background-color: #81baf9;
+  box-shadow: 1px 2px 10px rgba(0, 0, 0, 0.2);
+  color: white;
+  padding: 6.5px;
+  margin-top: 5px;
+  cursor: pointer;
 `;
